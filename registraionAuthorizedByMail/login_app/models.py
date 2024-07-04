201ad57d12f97d22b9bb06f694247fc3ee19d9e6
Al-amen <alamen15@cse.pstu.ac.bd>
2024-07-05 02:08:54 +0600

    Create Profile Model

--- registraionAuthorizedByMail/login_app/models.py
+++ registraionAuthorizedByMail/login_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_verified = models.BooleanField(default=False)
+    auto_token = models.CharField(max_length=200)
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.user.username
